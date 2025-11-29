1e0877839e15c46566bc50f633a2c6f4deff65f5
Gelly <xiaorichie1987@126.com>
2025-11-29 15:54:50 +0800

    Initial Commit

---
+++ about lifeTests/about_lifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  about_lifeTests.swift
+//  about lifeTests
+//
+//  Created by Gelly on 2025/11/29.
+//
+
+import Testing
+@testable import about_life
+
+struct about_lifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
